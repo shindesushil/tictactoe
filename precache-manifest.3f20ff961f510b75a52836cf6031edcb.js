b824a1f444d0fe152ec16b34a9298e716f994a6b
Sushil Shinde <sushilshinde9158@gmail.com>
2020-11-30 11:16:35 +0530

    Updates

---
+++ precache-manifest.3f20ff961f510b75a52836cf6031edcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b1dac6cc930bb14daa20865f0bddcce",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "fed6077b91d5145cb6e9",
+    "url": "/tictactoe/static/css/2.1f35d23a.chunk.css"
+  },
+  {
+    "revision": "315852a2c06f863dc4e2",
+    "url": "/tictactoe/static/css/main.0ede2f65.chunk.css"
+  },
+  {
+    "revision": "fed6077b91d5145cb6e9",
+    "url": "/tictactoe/static/js/2.064eb08c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tictactoe/static/js/2.064eb08c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "315852a2c06f863dc4e2",
+    "url": "/tictactoe/static/js/main.8da57832.chunk.js"
+  },
+  {
+    "revision": "4434fef27fe19069e69b",
+    "url": "/tictactoe/static/js/runtime-main.666fab5a.js"
+  }
+]);
